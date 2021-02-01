800d3d8c4bc9043d498704caeb3729df3e8538dd
cmonaccio-wcg <cmonaccio@westcarygroup.com>
2021-02-01 17:08:31 -0500

    Initial Commit

---
+++ helloworldapp/helloworldappApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldappApp.swift
+//  helloworldapp
+//
+//  Created by Chris Monaccio on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct helloworldappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
