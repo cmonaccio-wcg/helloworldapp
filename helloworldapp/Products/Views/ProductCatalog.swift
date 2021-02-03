d28cd965c1d2177a942c5c2eb20a2ce3e6810d95
cmonaccio-wcg <cmonaccio@westcarygroup.com>
2021-02-03 18:06:35 -0500

    feat: add ProductCatalog stub

---
+++ helloworldapp/Products/Views/ProductCatalog.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCatalog.swift
+//  helloworldapp
+//
+//  Created by Chris Monaccio on 2/3/21.
+//
+
+import SwiftUI
+
+struct ProductCatalog: View {
+    var body: some View {
+        Text("Product Catalog")
+    }
+}
+
+struct ProductCatalog_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCatalog()
+    }
+}
