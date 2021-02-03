25894389261de48e02c2ffcf39472663038eb45d
cmonaccio-wcg <cmonaccio@westcarygroup.com>
2021-02-03 18:05:47 -0500

    feat: add SearchView stub

---
+++ helloworldapp/Search/Views/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  helloworldapp
+//
+//  Created by Chris Monaccio on 2/3/21.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search View")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
